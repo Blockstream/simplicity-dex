b4d59005b753e9a3b359abd2ecc7cc48ad6b6954
Illia Kripaka <kripakaillya@gmail.com>
2025-12-15 20:13:04 +0200

    try to transfer assets to p2pk address

---
+++ crates/lwk-utils/tests/p2pk_faucet.rs
@@ -0,0 +1,64 @@
+use anyhow::anyhow;
+use lwk_common::Signer;
+use lwk_wollet::Wollet;
+use lwk_wollet::asyncr::EsploraClient;
+use simplicity::elements::{Address, AssetId};
+
+pub async fn faucet_p2pk_asset(
+    client: &mut EsploraClient,
+    signer: &impl Signer,
+    wollet: &mut Wollet,
+    recipient_address: &Address,
+    amount: u64,
+    asset: AssetId,
+) -> anyhow::Result<String> {
+    let update = client
+        .full_scan(wollet)
+        .await
+        .map_err(|e| anyhow!("Full scan failed: {}", e))?;
+
+    if let Some(update) = update {
+        wollet
+            .apply_update(update)
+            .map_err(|e| anyhow!("Apply update failed: {}", e))?;
+    }
+
+    let mut builder = wollet.tx_builder();
+
+    let is_confidential = recipient_address.to_string().starts_with("lq1");
+
+    if is_confidential {
+        builder = builder
+            .add_recipient(recipient_address, amount, asset)
+            .map_err(|e| anyhow!("Failed to add recipient: {}", e))?;
+    } else {
+        builder = builder
+            .add_explicit_recipient(recipient_address, amount, asset)
+            .map_err(|e| anyhow!("Failed to add explicit recipient:  {}", e))?;
+    }
+
+    // Build and sign the transaction
+    let mut unsigned_pset = builder
+        .finish()
+        .map_err(|e| anyhow!("Failed to build transaction: {}", e))?;
+
+    let signed_pset = signer
+        .sign(&mut unsigned_pset)
+        .map_err(|e| anyhow!("Failed to sign transaction: {e:?}"))?;
+
+    // Finalize and extract transaction
+    let finalized_pset = wollet
+        .finalize(&mut unsigned_pset)
+        .map_err(|e| anyhow!("Failed to finalize transaction:  {}", e))?;
+
+    // Broadcast transaction
+    let txid = client
+        .broadcast(&finalized_pset)
+        .await
+        .map_err(|e| anyhow!("Failed to broadcast transaction: {e:?}"))?;
+
+    Ok(format!(
+        "Sent {} sats to address {} with transaction {}.",
+        amount, recipient_address, txid
+    ))
+}
--- crates/lwk-utils/tests/testing_faucet.rs
+++ crates/lwk-utils/tests/testing_faucet.rs
@@ -1,21 +1,28 @@
 mod faucet_contract;
+mod p2pk_faucet;
 mod utils;
 
 use crate::faucet_contract::{TxInfo, issue_asset};
+use crate::p2pk_faucet::faucet_p2pk_asset;
 use crate::utils::{
     TEST_LOGGER, TestWollet, generate_signer, get_descriptor, test_client_electrum, test_client_esplora,
     wait_update_with_txs,
 };
 use elements::bitcoin::bip32::DerivationPath;
+use elements::hex::ToHex;
 use lwk_signer::SwSigner;
 use lwk_test_util::{TestEnvBuilder, generate_view_key, regtest_policy_asset};
 use lwk_wollet::asyncr::EsploraClient;
 use lwk_wollet::blocking::BlockchainBackend;
+use lwk_wollet::elements_miniscript::ToPublicKey;
 use lwk_wollet::{ElementsNetwork, NoPersist, Wollet, WolletBuilder, WolletDescriptor};
 use nostr::secp256k1::Secp256k1;
 use simplicity::bitcoin::secp256k1::Keypair;
+use simplicityhl::elements::secp256k1_zkp::PublicKey;
 use simplicityhl::elements::{AddressParams, TxOut};
-use simplicityhl_core::{LIQUID_TESTNET_BITCOIN_ASSET, LIQUID_TESTNET_GENESIS, derive_public_blinder_key};
+use simplicityhl_core::{
+    LIQUID_TESTNET_BITCOIN_ASSET, LIQUID_TESTNET_GENESIS, derive_public_blinder_key, get_p2pk_address,
+};
 use std::str::FromStr;
 
 const DEFAULT_MNEMONIC: &str =
@@ -89,6 +96,13 @@ fn test_issue_custom2() -> anyhow::Result<()> {
 
     let address = wallet.wollet.address(Some(0))?;
     wallet.fund_btc(&env);
+
+    let public_key = keypair.x_only_public_key().0;
+    let p2pk_address = get_p2pk_address(&public_key, &AddressParams::LIQUID_TESTNET)?;
+
+    let txid = env.elementsd_sendtoaddress(&p2pk_address, 1_000_011, None);
+    println!("txid on p2pk address: {}", txid);
+
     // wallet.fund(
     //     &env,
     //     10_000_000,
@@ -102,39 +116,124 @@ fn test_issue_custom2() -> anyhow::Result<()> {
     let external_utxos = wallet.wollet.explicit_utxos()?;
     println!("external_utxos: {:?}", external_utxos);
 
-    // let mut pset = issue_asset(
-    //     &keypair,
-    //     derive_public_blinder_key().public_key(),
-    //     utxos[0].outpoint,
-    //     123456,
-    //     500,
-    //     &AddressParams::LIQUID_TESTNET,
-    //     LIQUID_TESTNET_BITCOIN_ASSET,
-    //     *LIQUID_TESTNET_GENESIS,
-    // )
-    // .await?;
-
-    // let mut pset = tokio::runtime::Runtime::new()?.block_on(async {
-    //     issue_asset(
-    //         &keypair,
-    //         derive_public_blinder_key().public_key(),
-    //         utxos[0].outpoint,
-    //         123456,
-    //         500,
-    //         &AddressParams::LIQUID_TESTNET,
-    //         LIQUID_TESTNET_BITCOIN_ASSET,
-    //         *LIQUID_TESTNET_GENESIS,
-    //     )
-    //         .await
-    // })?;
-
-    // let tx_to_send = wallet.wollet.finalize(&mut pset)?;
-    // wallet.client.broadcast(&tx_to_send)?;
-
     wallet.sync();
 
     let utxos = wallet.wollet.utxos()?;
     tracing::info!("Utxos after: {:?}", utxos);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_issue_custom2_p2pk() -> anyhow::Result<()> {
+    let _ = dotenvy::dotenv().ok();
+    let _guard = &*TEST_LOGGER;
+
+    let secp = Secp256k1::new();
+    let env = TestEnvBuilder::from_env().with_esplora().build();
+    let mut client = test_client_esplora(&env.esplora_url());
+
+    let signer = generate_signer();
+    let view_key = generate_view_key();
+    let regtest_bitcoin_asset = regtest_policy_asset();
+
+    let descriptor = format!("ct({},elwpkh({}/*))", view_key, signer.xpub());
+    let network = ElementsNetwork::default_regtest();
+    let descriptor: WolletDescriptor = descriptor.parse()?;
+    let mut wollet = WolletBuilder::new(network, descriptor).build()?;
+    let signer_keypair = Keypair::from_secret_key(&secp, &signer.derive_xprv(&DerivationPath::master())?.private_key);
+
+    let update = client.full_scan(&wollet).await?;
+    if let Some(update) = update {
+        wollet.apply_update(update)?;
+    }
+
+    println!("Utxos1: {:?}", wollet.utxos()?);
+
+    let address = wollet.address(None)?;
+    let txid = env.elementsd_sendtoaddress(address.address(), 2_000_011, None);
+    println!("txid sendtoaddress: {}", txid);
+
+    env.elementsd_generate(10);
+    let update = wait_update_with_txs(&mut client, &wollet).await;
+    wollet.apply_update(update)?;
+    println!("Utxos2: {:?}", wollet.utxos()?);
+
+    let public_key = signer_keypair.x_only_public_key().0;
+    let p2pk_address = get_p2pk_address(&public_key, &AddressParams::ELEMENTS)?;
+
+    env.elementsd_generate(10);
+    let update = client.full_scan(&wollet).await?;
+    if let Some(update) = update {
+        wollet.apply_update(update)?;
+    }
+
+    println!("Utxos3: {:?}", wollet.utxos()?);
+
+    let msg = faucet_p2pk_asset(
+        &mut client,
+        &signer,
+        &mut wollet,
+        &p2pk_address,
+        1_000_000,
+        regtest_policy_asset(),
+    )
+    .await?;
+    println!("txid on p2pk address: '{msg}'");
+    println!("Utxos4: {:?}", wollet.utxos()?);
+
+    let utxos = wollet.utxos()?;
+    println!("Utxos5: {:?}", utxos);
+    let asset_owned = wollet.assets_owned()?;
+    println!("asset_owned: {:?}", asset_owned);
+    let external_utxos = wollet.explicit_utxos()?;
+    println!("external_utxos: {:?}", external_utxos);
+
+    let update = client.full_scan(&wollet).await?;
+    if let Some(update) = update {
+        wollet.apply_update(update)?;
+    }
+
+    let utxos = wollet.utxos()?;
+    tracing::info!("Utxos after: {:?}", utxos);
+
+    // retrieve utxos from pt2tr wallet
+
+    let blinding_key = derive_public_blinder_key();
+    let view_key = blinding_key.secret_bytes().to_hex();
+    // let pk = signer_keypair.public_key().to_hex();
+    let pk = "020202020202020202020202020202020202020202020202020202020202020202";
+    let pubkey = PublicKey::from_str(pk).unwrap().to_x_only_pubkey();
+    let p2pk_address = simplicityhl_core::get_p2pk_address(&pubkey, &AddressParams::ELEMENTS)?;
+    println!("pk for p2pk_address: {}, p2pk addr: {}", pk, p2pk_address);
+
+    let desc = format!("ct({view_key},elwpkh({pk}))");
+    println!("desc: {}", desc);
+
+    let descriptor: WolletDescriptor = desc.parse()?;
+    let mut p2pk_wollet = WolletBuilder::new(network, descriptor).build()?;
+
+    let update = client.full_scan(&p2pk_wollet).await?;
+    if let Some(update) = update {
+        p2pk_wollet.apply_update(update)?;
+    }
+
+    println!("Utxos p2pk1: {:?}", p2pk_wollet.utxos()?);
+
+    //
+    // w.fund_btc(&env);
+    // let balance = w.balance_btc();
+    // assert!(balance > 0);
+    // let utxos = w.wollet.utxos().unwrap();
+    // assert_eq!(utxos.len(), 1);
+    //
+    // // Receive unconfidential / explicit
+    // let satoshi = 5_000;
+    // w.fund_explicit(&env, satoshi, None, None);
+    // assert_eq!(w.balance_btc(), balance);
+    //
+    // let explicit_utxos = w.wollet.explicit_utxos().unwrap();
+    // assert_eq!(explicit_utxos.len(), 1);
 
     Ok(())
 }
@@ -159,7 +258,7 @@ async fn async_test_issue_custom2() -> anyhow::Result<()> {
     let keypair = Keypair::from_secret_key(&secp, &signer.derive_xprv(&DerivationPath::master())?.private_key);
 
     let update = client.full_scan(&wollet).await?.unwrap();
-    wollet.apply_update(update).unwrap();
+    wollet.apply_update(update)?;
 
     let address = wollet.address(None)?;
     let txid = env.elementsd_sendtoaddress(address.address(), 1_000_011, None);
@@ -197,7 +296,7 @@ async fn async_test_issue_custom2() -> anyhow::Result<()> {
         TxInfo { outpoint, wallet_tx },
         123456,
         500,
-        &AddressParams::LIQUID_TESTNET,
+        &AddressParams::ELEMENTS,
         regtest_bitcoin_asset,
         *LIQUID_TESTNET_GENESIS,
     )
